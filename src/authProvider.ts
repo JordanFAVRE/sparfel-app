9222ed349220093c9c9bf77119b265da9a1c7108
Webdesign29 <ben@webdesign29.net>
2023-01-13 16:55:43 +0100

    Change user

--- src/authProvider.ts
+++ src/authProvider.ts
@@ -43,7 +43,7 @@ export const authProvider: AuthProvider = {
 
         return Promise.resolve({
             id: 1,
-            name: "Jordan Favre",
+            name: "Test user",
             avatar: "https://i.pravatar.cc/150",
         });
     },
